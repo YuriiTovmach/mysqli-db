aa9f3b9247e04d4257585f52a2be11f3520879b2
YuriiTovmach <portfolio_yurii_tovmach@ukr.net>
2020-05-04 13:26:41 +0300

    second

--- connect_db.php
+++ connect_db.php
@@ -8,7 +8,16 @@ if ($mysqli -> connect_errno){
 }
 
 if ($result = $mysqli -> query("SELECT * FROM oc_country")) {
-	echo "Returned rows are: " .$result ->num_rows;
+	echo "Returned rows are: " .$result ->num_rows . "<br>";
+
+$query = "SELECT name FROM oc_country";
+	if ($result = $mysqli->query($query)){
+		while($row = $result->fetch_row()){
+			// printf("%s (%s)\n", $row[0], $row[1]);
+			echo 'Страна: ' .$row[0]. "<br>";
+		}
+	}	
+
 
 	$result -> free_result();
 }
