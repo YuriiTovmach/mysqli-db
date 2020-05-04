5bb70290b178a885fcdba2dee2ee59a980cc37a9
YuriiTovmach <portfolio_yurii_tovmach@ukr.net>
2020-05-04 14:10:23 +0300

    second commit

--- connect_db.php
+++ connect_db.php
@@ -7,16 +7,30 @@ if ($mysqli -> connect_errno){
 	exit();
 }
 
-if ($result = $mysqli -> query("SELECT * FROM oc_country")) {
-	echo "Returned rows are: " .$result ->num_rows . "<br>";
+// if ($result = $mysqli -> query("SELECT * FROM oc_country")) {
+// 	echo "Returned rows are: " .$result ->num_rows . "<br>";
 
-$query = "SELECT name FROM oc_country";
-	if ($result = $mysqli->query($query)){
-		while($row = $result->fetch_row()){
-			// printf("%s (%s)\n", $row[0], $row[1]);
-			echo 'Страна: ' .$row[0]. "<br>";
+// $query = "SELECT name FROM oc_country";
+// 	if ($result = $mysqli->query($query)){
+// 		// while($row = $result->fetch_row()){
+// 			// printf("%s (%s)\n", $row[0], $row[1]);
+// 			// echo 'Страна: ' .$row[0]. "<br>";
+
+// 			while($row = $result->fetch_array()){
+// 				echo 'Страна: ' .$row[0]. "<br>";
+// 		}
+// 	}	
+
+if ($result = $mysqli-> query("SELECT country_id, name FROM oc_country ")) {
+	// $row = $result-> fetch_assoc();
+	// printf ("%s (%s)\n", $row["country_id"], $row["name"]);
+
+	
+		 while($row = $result->fetch_assoc()){
+			echo $row["country_id"] . " " .$row["name"] .'<br>' ;
 		}
-	}	
+			
+// }
 
 
 	$result -> free_result();
